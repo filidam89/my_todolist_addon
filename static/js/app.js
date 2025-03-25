9593fd266131667f21b1878f19649aa71003aa5d
filidam89 <54314342+filidam89@users.noreply.github.com>
2025-03-25 19:00:58 +0100

    Create app.js

---
+++ static/js/app.js
@@ -0,0 +1,3 @@
+// Se si desidera implementare funzionalità AJAX per aggiornare dinamicamente la lista dei task, 
+// è possibile utilizzare questo file per recuperare dati dall'endpoint /api/tasks
+// ed aggiornare la pagina senza doverla ricaricare.
